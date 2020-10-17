8b4cb2c1cf3bdd6cea92545ce28eac1594a918f7
Matouš Marek <mylapqn312@gmail.com>
2020-10-18 01:55:48 +0200

    f

--- client.js
+++ client.js
@@ -9,38 +9,51 @@ document.body.appendChild(app.renderer.view);
 app.renderer.view.width = window.innerWidth;
 app.renderer.view.height = window.innerHeight;
 app.renderer.resize(window.innerWidth, window.innerHeight);
-app.renderer.backgroundColor = 0xFFE793;
+app.renderer.backgroundColor = 0x1E1E1E;
+
+
 
 window.addEventListener("resize", function () {
     app.renderer.resize(window.innerWidth, window.innerHeight);
 });
 
 loader
-    .add("player0", "images/player0.png");
+    .add("player0", "images/player0.png")
+    .add("kour", "images/kour.png")
+    .add("spark","images/spark.png")
+    ;
 loader.onProgress.add(loadingProgress);
 loader.load(start);
 
 var playerSprite;
-var loaded=false;
-var connected=false;
-var running=false;
+var loaded = false;
+var connected = false;
+var running = false;
 
+let particleContainer = new PIXI.ParticleContainer();
+particleContainer.maxSize=10000;
+//particleContainer.blendMode = PIXI.BLEND_MODES.ADD;
+var particleSystem;
 function start() {
     playerSprite = new PIXI.Sprite(loader.resources.player0.texture);
     document.getElementById("loadingBarContainer").style.display = "none";
     playerSprite.scale.set(0.5);
     playerSprite.anchor.set(0.5);
+    app.stage.addChild(particleContainer);
     app.stage.addChild(playerSprite);
-    loaded = true;
+
+    particleSystem = new ParticleSystem();
+
     app.ticker.add(graphicsUpdate);
+    loaded = true;
     connect();
 }
 function loadingProgress(e) {
-    document.getElementById("loadingBar").style.width = e.progress+"%";
+    document.getElementById("loadingBar").style.width = e.progress + "%";
     console.log("loading", e.progress);
 }
 
-function connect(){
+function connect() {
     if (window.location.hostname.length == 0) {
         //connection = new WebSocket("ws://localhost:20003/");
         connection = new WebSocket("wss://jumpout.ws.coal.games/");
@@ -62,6 +75,16 @@ var localPlayer = new Player();
 localPlayer.init();
 
 
+
+function updateParticles(deltaTime) {
+    if (running) {
+        if(localPlayer.ship.control.y == 0) particleSystem.settings.emitRate = 0;
+        else particleSystem.settings.emitRate = 10;
+        particleSystem.setEmitter(localPlayer.ship.position, localPlayer.ship.velocity, localPlayer.ship.rotation);
+        particleSystem.update(deltaTime);
+    }
+}
+
 function onConnectionClose(e) {
     console.log("Connection closed. Code: " + e.code + " Reason: " + e.reason);
 }
@@ -82,11 +105,15 @@ function onConnectionMessage(messageRaw) {
 function parseMessage(message) {
     const view = new DataView(message);
     let index = { i: 0 };
-    parsePlayer(view, index, localPlayer);
-    let controlX = view.getFloat32(index.i);
-    index.i += 4;
-    let controlY = view.getFloat32(index.i);
-    index.i += 4;
+    let messageType = view.getUint8(index.i);
+    index.i += 1;
+    switch (messageType) {
+        case 1:
+            parsePlayer(view, index);
+            break;
+
+    }
+
     //console.log("controlX: " + controlX + " Y:" + controlY + "struct on the next line");
     //console.log(localPlayer);
     playerSprite.x = localPlayer.ship.position.x;
@@ -94,7 +121,12 @@ function parseMessage(message) {
     playerSprite.rotation = localPlayer.ship.rotation;
 }
 
-function parsePlayer(view, index, player) {
+function parsePlayer(view, index) {
+    let id = view.getUint8(index.i);
+    index.i += 1;
+    //let player = Player.findID(id);
+    let player = localPlayer;
+
     player.ship.position.x = view.getFloat32(index.i);
     index.i += 4;
     player.ship.position.y = view.getFloat32(index.i);
@@ -105,25 +137,32 @@ function parsePlayer(view, index, player) {
     index.i += 4;
     player.ship.rotation = view.getFloat32(index.i);
     index.i += 4;
+    player.ship.control.x = view.getFloat32(index.i);
+    index.i += 4;
+    player.ship.control.y = view.getFloat32(index.i);
+    index.i += 4;
 }
 
-const fps = 30;
+const fps = 60;
 
 setInterval(update, 1000 / fps);
 
 function update() {
-    if(running){
-    sendControls();
+    if (running) {
+        sendControls();
     }
 }
 
+var fpsText = new PIXI.Text();
+app.stage.addChild(fpsText);
 
-
-function graphicsUpdate(deltaTimeMs){
-    let deltaTime = deltaTimeMs/1000;
-    playerSprite.x += localPlayer.ship.velocity.x*deltaTime*20;
-    playerSprite.y += localPlayer.ship.velocity.y*deltaTime*20;
-    console.log(localPlayer.ship.velocity);
+function graphicsUpdate(deltaTimeFactor) {
+    let deltaTime = app.ticker.deltaMS / 1000;
+    fpsText.text = app.ticker.FPS + " FPS\n" + app.ticker.minFPS + " Min FPS\n" + app.ticker.maxFPS + " Max FPS\n" + deltaTimeFactor + " Factor\n";
+    playerSprite.x += localPlayer.ship.velocity.x * deltaTime;
+    playerSprite.y += localPlayer.ship.velocity.y * deltaTime;
+    //console.log(localPlayer.ship.velocity);
+    updateParticles(deltaTime);
 }
 
 let controlVector = { x: 0, y: 0 };
